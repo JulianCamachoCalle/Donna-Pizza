1cb0db7702de68438ca84a3ac85c7ed7009bd0bb
Julian Fernando Camacho Calle <u22242383@utp.edu.pe>
2024-10-25 18:00:19 -0500

    FINISH CRUD CLIENTES HTML

--- DonnaPizza/src/main/java/com/example/DonnaPizza/controladores/ControladorPrincipal.java
+++ DonnaPizza/src/main/java/com/example/DonnaPizza/controladores/ControladorPrincipal.java
@@ -1,13 +1,15 @@
 package com.example.DonnaPizza.controladores;
 
 
-
+import com.example.DonnaPizza.Services.ServicioCliente;
 import jakarta.servlet.http.HttpServletRequest;
-import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import java.util.Locale;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,34 +25,10 @@ public class ControladorPrincipal {
     public String index(Model model) {
         return "index";
     }
-    
-
-    
-
-    @GetMapping({"/contacto"})
-    public String contacto(Model model) {
-        return "contacto";
-    }
-   @RequestMapping("/dataformcontacto")
-    public String dataformcontacto(@RequestParam("nombre") String nombre,
-            @RequestParam("email") String email,
-            @RequestParam("telefono") String telefono,
-            Model model){
-        
-        model.addAttribute("nombre", nombre);
-        model.addAttribute("email", email);
-        model.addAttribute("telefono", telefono);
-        return "listacontactos";
-    }
 
     @GetMapping("/carta")
     public String carta(Model model) {
         return "carta";
-    }
-
-    @GetMapping("/membresia")
-    public String membresia(Model model) {
-        return "membresia";
     }
 
     @GetMapping("/locales")
@@ -63,68 +41,37 @@ public class ControladorPrincipal {
         return "login";
     }
 
-    //registro
-    @RequestMapping("/dataFormRegistro")
-    public String dataFormRegistro(@RequestParam("nombre") String nombre,
-            @RequestParam("apellidos") String apellidos,
-            @RequestParam("correo") String correo,
-            @RequestParam("numero") String numero,
-            @RequestParam("cumpleanos") LocalDate cumpleanos,
-            @RequestParam("contrasena") String contrasena,
-            Model model
-    ) {
-        model.addAttribute("nombre", nombre);
-        model.addAttribute("apellidos", apellidos);
-        model.addAttribute("correo", correo);
-        model.addAttribute("numero", numero);
-        model.addAttribute("cumpleanos", cumpleanos);
-        model.addAttribute("contrasena", contrasena);
-        return "usuario";
-    }
-
     @GetMapping("/changeLanguage")
     public String changeLanguage(@RequestParam("lang") String lang, HttpServletRequest request) {
         request.getSession().setAttribute("org.springframework.web.servlet.i18n.SessionLocaleResolver.LOCALE", new Locale(lang));
         return "redirect:" + request.getHeader("Referer");
     }
 
-   
+    private final ServicioCliente servicioCliente;
+    public ControladorPrincipal(ServicioCliente servicioCliente) {
+        this.servicioCliente = servicioCliente;
+    }
 
-    @GetMapping("/sugerencias")
-    public String sugerencias(Model model) {
-        return "sugerencias";
+    @GetMapping("/clientes")
+    public String listarClientes(Model model) {
+        model.addAttribute("clientes", servicioCliente.getClientes());
+        return "CRUDClientes";
     }
-    @RequestMapping("/dataformsugerencia")
-    public String dataformsugerencia(@RequestParam("nombre") String nombre,
-            @RequestParam("email") String email,
-            @RequestParam("descripcion") String descripcion,
-            Model model){
-        
-        model.addAttribute("nombre", nombre);
-        model.addAttribute("email", email);
-        model.addAttribute("descripcion", descripcion);
-        return "listasugerencias";
-    }
-    
 
-    @GetMapping("/reclamaciones")
-    public String reclamaciones(Model model) {
-        return "reclamaciones";
-    }
-    
+
     @GetMapping("/fromclient")
     public String fromclient(Model model) {
         return "fromclient";
     }
-    
+
     //clientes
     @RequestMapping("/dataclientfrom")
     public String dataclientfrom(@RequestParam("nombre") String nombre,
-            @RequestParam("apellidos") String apellidos,
-            @RequestParam("correo") String correo,
-            @RequestParam("numero") String numero,
-            @RequestParam("direccion") String direccion,
-            Model model
+                                 @RequestParam("apellidos") String apellidos,
+                                 @RequestParam("correo") String correo,
+                                 @RequestParam("numero") String numero,
+                                 @RequestParam("direccion") String direccion,
+                                 Model model
     ) {
         model.addAttribute("nombre", nombre);
         model.addAttribute("apellidos", apellidos);
@@ -133,10 +80,10 @@ public class ControladorPrincipal {
         model.addAttribute("direccion", direccion);
         return "client";
     }
-    
+
     @GetMapping("/especificaciones")
     public String especificaciones(Model model) {
         return "especificaciones";
     }
-    
+
 }
--- DonnaPizza/src/main/java/com/example/DonnaPizza/webConfiguraciones/webConfig.java
+++ DonnaPizza/src/main/java/com/example/DonnaPizza/webConfiguraciones/webConfig.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -29,5 +30,12 @@ public class webConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*");
     }
 }
