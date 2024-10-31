3a0d9dee0b71db28e5875ddd7839f949210963b1
RoosveltMM <roosveltowen2002terceracuenta@gmail.com>
2024-10-31 02:36:48 -0500

    cree el CRUD de metodos de pago

---
+++ DonnaPizza/src/main/java/com/example/DonnaPizza/Repository/MetodosPagoRepository.java
@@ -0,0 +1,14 @@
+package com.example.DonnaPizza.Repository;
+
+import com.example.DonnaPizza.Model.MetodosPago;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ImetodosPagoRepository extends JpaRepository<metodosPago, Long> {
+
+    // Buscar Segun Nombre
+    Optional<MetodosPago> findByNombre(String nombre);
+}
